4a2b1eeee740335bd6672def35534811bdc088af
alisa-lin <alisalin610@gmail.com>
2020-06-23 17:10:18 -0400

    enclose in router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,37 +9,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>My Token = {window.token}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Switch>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <Route path="/sign-up">
-          <SignUpPage />
-        </Route>
-        <Route path="/dashboard">
-          <Dashboard />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-      </Switch>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/sign-up">
+            <SignUpPage />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
