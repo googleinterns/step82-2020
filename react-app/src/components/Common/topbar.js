f042393feaad8f921c7d3d1145bc7b3e8f5e9898
alisa-lin <alisalin610@gmail.com>
2020-06-26 16:26:32 -0400

    add buttons and title to topbar

--- react-app/src/components/Common/topbar.js
+++ react-app/src/components/Common/topbar.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import '../../index.css';
-import { Layout } from 'antd';
+import { Layout, Menu, Input, Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
 const { Header } = Layout;
+const { Search } = Input;
 
 const Topbar = () => {
   return (
-    <Header className="topbar" />
+    <Header className="topbar-wrapper">
+      <Menu theme="dark" mode="inline">
+        <Search
+          placeholder="Search in Title..."
+          onSearch={value => console.log(value)}
+          className="top-search"
+        />
+        <UserOutlined className="top-options" />
+        <Button className="top-options" type="primary">Log out</Button>
+      </Menu>
+      <Menu theme="dark" mode="inline">
+        <h1>TITLE</h1>
+      </Menu>
+    </Header>
   )
 }
 
