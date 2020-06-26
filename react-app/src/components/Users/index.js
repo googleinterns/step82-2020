48d6e16c6eeefc24b0e814953b0ef0a4782259d5
alisa-lin <alisalin610@gmail.com>
2020-06-26 11:22:12 -0400

    move sidebar to common

--- react-app/src/components/Users/index.js
+++ react-app/src/components/Users/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import 'antd/dist/antd.css';
+import '../../index.css';
+import { Layout } from 'antd';
+import Sidebar from '../Common/sidebar'
 
-const Users = () => <div>Users</div>
+const { Header, Content, Footer } = Layout;
+
+const Users = () => {
+  return (
+    <Layout>
+      <Sidebar />
+      <Layout className="site-layout" style={{ marginLeft: '200px' }}>
+        <Header style={{ position: 'relative', height: '153px' }} />
+        <Content style={{ position: 'relative', margin: '24px 16px 0', overflow: 'initial' }}>
+          <div className="site-layout-background" style={{ padding: '24px', textAlign: 'center' }}>
+
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Clink</Footer>
+      </Layout>
+    </Layout>
+  );
+}
 
 export default Users;
