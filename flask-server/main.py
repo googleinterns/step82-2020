88b489f0be0e81c14a44bfc2f17452ef29d126b6
Mars Wang <marswang92@gmail.com>
2020-06-28 13:23:10 -0700

    Updated readme.

--- flask-server/main.py
+++ flask-server/main.py
@@ -2,7 +2,6 @@ from flask import (Flask, render_template)
 
 app = Flask(__name__, template_folder='static/react')
 
-
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
