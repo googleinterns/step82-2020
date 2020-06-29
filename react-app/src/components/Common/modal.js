253923fddc65eff7e7d85733b4a309cbdbc16319
michaeleggboy <michaeleggboy@google.com>
2020-06-29 16:29:05 -0400

    + new button opens up a modal

---
+++ react-app/src/components/Common/modal.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import '../../index.css';
+import { PlusOutlined } from '@ant-design/icons';
+import { Modal, Button } from 'antd';
+
+class ModalWindow extends React.Component {
+
+  state = {
+    loading: false,
+    visible: false,
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = () => {
+    this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false });
+    }, 3000);
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
+  render() {
+    const { visible, loading } = this.state;
+    return (
+      <div>
+        <Button className="new-button" type="primary" icon={<PlusOutlined />} onClick={this.showModal}>
+          New
+        </Button>
+        <Modal
+          visible={visible}
+          title="Title"
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          footer={[
+            <Button key="back" onClick={this.handleCancel}>
+              Return
+            </Button>,
+            <Button key="submit" type="primary" loading={loading} onClick={this.handleOk}>
+              Submit
+            </Button>,
+          ]}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default ModalWindow
--- react-app/src/components/Common/sidebar.js
+++ react-app/src/components/Common/sidebar.js
@@ -2,18 +2,16 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import '../../index.css';
 import { Layout, Menu } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import ModalWindow from './modal';
 
 const { Sider } = Layout;
 
 const Sidebar = () => {
   return (
-    <Sider className = "sidebar">
+    <Sider className="sidebar">
       <Menu theme="dark" mode="inline">
         <div className="logo" />
-        <Menu.Item key="new" icon={<PlusOutlined />}>
-          New
-      </Menu.Item>
+        <ModalWindow />
       </Menu>
       <Menu className = "sidebar-scroll" theme="dark" mode="inline" defaultSelectedKeys={['all']} >
         <Menu.Item key="all">
