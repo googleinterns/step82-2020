857f7f3af30f863a5a48aaca987ee6f955559921
michaeleggboy <michaeleggboy@google.com>
2020-06-30 17:26:36 -0400

    lowercased all instances of Email

--- react-app/src/components/Auth/SignUpPage/index.js
+++ react-app/src/components/Auth/SignUpPage/index.js
@@ -38,11 +38,11 @@ const SignUpPage = () => {
         rules={[
           {
             type: 'email',
-            message: 'The input is not valid E-mail!',
+            message: 'The input is not valid email!',
           },
           {
             required: true,
-            message: 'Please input your E-mail!',
+            message: 'Please input your email!',
           },
         ]}
       >
