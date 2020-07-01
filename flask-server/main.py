6763c3a50dcc6bbb434e196cb78b1164eb8baa5b
michaeleggboy <michaeleggboy@google.com>
2020-07-01 18:47:17 -0400

    updated main.py

--- flask-server/main.py
+++ flask-server/main.py
@@ -9,7 +9,8 @@ from google.cloud import datastore
 datastore_client = datastore.Client()
 app = Flask(__name__, template_folder='static/react')
 bcrypt = Bcrypt()
-SECRET_KEY = os.getenv('SECRET_KEY', 'zrRjR8zj00RIxf-8_Z6iiJow7uDuk9023hr0i')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 def store_time(dt):
     entity = datastore.Entity(key=datastore_client.key('visit'))
@@ -69,10 +70,7 @@ def show_users():
     return jsonify(array)
 
 # check user login data
-@app.route('/login', methods=['POST'])
-def check_user():
-    return login_user(request.json['username'], request.json['password'])
-
+@app.route('/apis/login', methods=['POST'])
 def login_user(username, password):
     try: 
         query = datastore_client.query(kind='user').add_filter('username', '=', username) # clean up code
@@ -83,7 +81,7 @@ def login_user(username, password):
                     response_object = {
                         'status': 'success',
                         'message': 'Successfully logged in.',
-                        'Authorization': decode_auth_token(auth_token)
+                        'Authorization': auth_token.decode()
                     }
                     return response_object, 200
         else:
@@ -117,7 +115,7 @@ def encode_auth_token(username):
         }
         return jwt.encode(
             payload,
-            SECRET_KEY, # key is properly defined
+            SECRET_KEY,
             algorithm='HS256'
         )
     except Exception as e:
@@ -130,7 +128,7 @@ def decode_auth_token(auth_token):
     :return: integer|string
     """
     try:
-        payload = jwt.decode(auth_token, SECRET_KEY) # key isn't properly defined
+        payload = jwt.decode(auth_token, SECRET_KEY)
         return payload['sub']
     except jwt.ExpiredSignatureError:
         return 'Signature expired. Please log in again.'
