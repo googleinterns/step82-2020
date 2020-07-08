383dea15134dafcdf016e70b734a6caeab269105
alisa-lin <alisalin610@gmail.com>
2020-07-08 14:41:59 -0400

    add bookmark modal to bookmark cards

--- react-app/src/components/Common/card.js
+++ react-app/src/components/Common/card.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import 'antd/dist/antd.css';
 import '../../index.css';
 import { Collapse } from 'antd';
+import BookmarkModal from './bookmarkmodal'
 
 const { Panel } = Collapse;
 
@@ -10,7 +11,7 @@ const Card = () => {
 
   return(
     <Collapse accordion className="card" bordered={false} activeKey={activeKey}>
-      <Panel style={{ border: '0px'}} showArrow={false} header={<div onMouseEnter={() => setActiveKey('1')} onMouseLeave={() => setActiveKey('0')}>This is a card header</div>} key="1">
+      <Panel style={{ border: '0px'}} showArrow={false} header={<div onMouseEnter={() => setActiveKey('1')} onMouseLeave={() => setActiveKey('0')}>This is a card header <BookmarkModal /></div>} key="1">
         <p>Card info</p>
       </Panel>
     </Collapse>
