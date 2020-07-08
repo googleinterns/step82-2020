4bf7a6755b1883e7213a7fbe78426f8057875211
alisa-lin <alisalin610@gmail.com>
2020-07-08 14:09:48 -0400

    move clink menu items to single file

--- react-app/src/components/Common/topbar.js
+++ react-app/src/components/Common/topbar.js
@@ -2,14 +2,14 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import '../../index.css';
 import { Layout } from 'antd';
-import EllipsisMenuButton from './ellipsismenubutton';
+import ClinkMenu from './clinkmenu';
 
 const { Header } = Layout;
 
 const Topbar = () => {
   return (
     <Header style={{ position: 'relative', height: '153px' }} >
-      <EllipsisMenuButton />
+      <ClinkMenu />
     </Header>
   )
 }
