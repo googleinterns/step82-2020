3c9ae0dca7ed479b176eb025848840a983dfd7b7
alisa-lin <alisalin610@gmail.com>
2020-07-09 12:38:59 -0400

    change bookmark modal to menu

--- react-app/src/components/Common/card.js
+++ react-app/src/components/Common/card.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import 'antd/dist/antd.css';
 import '../../index.css';
 import { Collapse } from 'antd';
-import BookmarkModal from './bookmarkmodal'
+import BookmarkMenu from './bookmarkmenu'
 
 const { Panel } = Collapse;
 
@@ -11,7 +11,7 @@ const Card = () => {
 
   return(
     <Collapse accordion className="card" bordered={false} activeKey={activeKey}>
-      <Panel style={{ border: '0px'}} showArrow={false} header={<div className="card-header" onMouseEnter={() => setActiveKey('1')} onMouseLeave={() => setActiveKey('0')}>This is a card header <BookmarkModal /></div>} key="1">
+      <Panel style={{ border: '0px'}} showArrow={false} header={<div className="card-header" onMouseEnter={() => setActiveKey('1')} onMouseLeave={() => setActiveKey('0')}>This is a card header <BookmarkMenu /></div>} key="1">
         <p>Card info</p>
       </Panel>
     </Collapse>
