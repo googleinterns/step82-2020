4fba946a76a3d7917242c48ec3d805124fd21d10
michaeleggboy <michaeleggboy@google.com>
2020-07-10 10:38:30 -0400

    logout button moves left as window browser

--- react-app/src/components/Common/topbar.js
+++ react-app/src/components/Common/topbar.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import '../../index.css';
-import { Layout } from 'antd';
+import { Layout, Input, Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
 const { Header } = Layout;
+const { Search } = Input;
 
 const Topbar = () => {
+
   return (
-    <Header style={{ position: 'relative', height: '153px' }} />
+    <Header className="topbar">
+      <div className="topbar-searchbar-wrapper">
+        <div className="topbar-searchbar-container">
+          <Search
+            className="topbar-search"
+            placeholder="Search in Title..."
+            onSearch={value => console.log(value)}
+          />
+          <Button className="topbar-button" type="primary">Log Out</Button>
+          <UserOutlined className="topbar-user-icon" />
+        </div>
+      </div>
+      <h1 className="topbar-title">TITLE</h1>
+    </Header>
   )
 }
 
