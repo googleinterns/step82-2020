cf810a702d699088df8584dfa61b045df6497dc6
Claire <claireche@google.com>
2020-07-10 15:15:16 -0400

    create post request to add bookmark

--- react-app/src/features/apis.js
+++ react-app/src/features/apis.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:5000/apis';
 const LOGIN_URL = `${BASE_URL}/login`;
 const ADD_CLINK_URL = `${BASE_URL}/add-clink`;
+const ADD_BOOKMARK_URL = `${BASE_URL}/add-bookmark`;
 
 const login = (username, password) =>
   axios.post(LOGIN_URL, {username, password});
@@ -11,6 +12,13 @@ const addClink = (title) => axios.post(ADD_CLINK_URL, {
   title: title
 });
 
+const addBookmark = (link, title, description, clink) => axios.post(ADD_BOOKMARK_URL, {
+  link: link,
+  title: title,
+  description: description,
+  clink: clink
+});
+
 export default {
-  login, addClink
+  login, addClink, addBookmark
 }
