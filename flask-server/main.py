ea0539ba7c7bc781a210ce37d229b552e63fcd60
michaeleggboy <michaeleggboy@google.com>
2020-07-14 15:28:18 -0400

    fixed index error

--- flask-server/main.py
+++ flask-server/main.py
@@ -20,8 +20,8 @@ def store_user():
     email = request.json['email']
     username = request.json['username']
 
-    email_result = list(datastore_client.query(kind='user').add_filter('email', '=', email).fetch(limit=1))[0]
-    username_result = list(datastore_client.query(kind='user').add_filter('username', '=', username).fetch(limit=1))[0]
+    email_result = list(datastore_client.query(kind='user').add_filter('email', '=', email).fetch(limit=1))
+    username_result = list(datastore_client.query(kind='user').add_filter('username', '=', username).fetch(limit=1))
 
     if email_result and username_result:
         response_object = {
