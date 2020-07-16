f051e5787dbb6668eadf814a274b71fc77d0ea76
Claire <claireche@google.com>
2020-07-16 11:33:32 -0400

    fix card display

--- react-app/src/components/Common/card.js
+++ react-app/src/components/Common/card.js
@@ -1,4 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {fetchBookmarks} from '../../features/clink';
 import 'antd/dist/antd.css';
 import '../../index.css';
 import { Collapse } from 'antd';
@@ -8,21 +10,39 @@ const { Panel } = Collapse;
 
 const Card = () => {
   const [activeKey, setActiveKey] = useState('');
+  const currentToken = localStorage.getItem('currentToken')
+  const bookmarks = useSelector(state => state.clink.bookmarks)
+  const isCurrentUserFetched = useSelector(state => state.users.isCurrentUserFetched)
+  const clinkId = 'All';
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (isCurrentUserFetched) {
+      dispatch(fetchBookmarks(currentToken, clinkId))
+    }
+  }, [])
 
   return(
-    <div onMouseEnter={() => {
-      setActiveKey('1')
-    }
-    } onMouseLeave={() => {
-      setActiveKey('0')
-    }
-    }> 
-    <Collapse accordion activeKey={activeKey} className="card" bordered={false}>
-      <Panel style={{ border: '0px'}} showArrow={false} header={<div className="card-header">This is a card header <BookmarkMenu /></div>} key="1">
-        <p>Card info</p>
-      </Panel>
-    </Collapse>
-    </div>
+    <>
+    {bookmarks.map(bmark => (
+      <>
+      <div onMouseEnter={() => {
+        setActiveKey(bmark.id)
+      }
+      } onMouseLeave={() => {
+        setActiveKey('0')
+      }
+      }> 
+        <Collapse accordion activeKey={activeKey} className="card" bordered={false}>
+          <Panel style={{ border: '0px'}} showArrow={false} header={<div className="card-header">{bmark.title}<BookmarkMenu /></div>} key={bmark.id}>
+            <p>{bmark.description}</p>
+          </Panel>
+        </Collapse>
+        <br />
+      </div>
+      </>
+    ))}
+    </>
   )
 }
 
