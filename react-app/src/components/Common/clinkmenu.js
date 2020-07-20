deea954c8919dbb2524e03960ddb810a189adbf2
alisa-lin <alisalin610@gmail.com>
2020-07-20 16:26:42 -0400

    refactor clinkmenu

--- react-app/src/components/Common/clinkmenu.js
+++ react-app/src/components/Common/clinkmenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 import '../../index.css';
 import { EllipsisOutlined } from '@ant-design/icons';
@@ -11,135 +11,129 @@ const layout = {
 };
 
 // interactions with clinks
-class ClinkMenu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: false, editVisible: false, shareVisible: false, };
-  }
+const ClinkMenu = () => {
+  const [isLoading, setLoading] = useState(false);
+  const [editIsVisible, setEditVisible] = useState(false);
+  const [shareIsVisible, setShareVisible] = useState(false);
 
-  showEdit = () => {
-    this.setState({
-      editVisible: true,
-    });
+  const showEdit = () => {
+    setEditVisible(true);
   };
 
-  showShare = () => {
-    this.setState({
-      shareVisible: true,
-    });
+  const showShare = () => {
+    setShareVisible(true);
   };
 
-  onEditFinish = values => {
-    console.log('Success:', values);
-    this.setState({ loading: true });
+  const onEditFinish = values => {
+    setLoading(true);
     setTimeout(() => {
-      this.setState({ loading: false, editVisible: false });
+      setLoading(false);
+      setEditVisible(false);
+      setShareVisible(false);
     }, 3000);
   };
 
-  onShareFinish = values => {
-    console.log('Success:', values);
-    this.setState({ loading: true });
+  const onShareFinish = values => {
+    setLoading(true);
     setTimeout(() => {
-      this.setState({ loading: false, shareVisible: false });
+      setLoading(false);
+      setShareVisible(false);
+      setEditVisible(false);
     }, 3000);
   };
 
-  onFinishFailed = errorInfo => {
+  const onFinishFailed = errorInfo => {
     console.log('Failed:', errorInfo);
   };
 
-  handleCancel = () => {
-    this.setState({ editVisible: false, shareVisible: false });
+  const handleCancel = () => {
+    setEditVisible(false);
+    setShareVisible(false);
   };
 
-  render() {
-    const { editVisible, shareVisible, loading } = this.state;
+  const menu = (<Menu>
+    <Menu.Item key="edit" onClick={showEdit}>
+      Edit
+    </Menu.Item>
+    <Menu.Item key="delete">
+      Delete
+    </Menu.Item>
+    <Menu.Item key="share" onClick={showShare}>
+      Share
+    </Menu.Item>
+  </Menu>);
 
-    const menu = (<Menu>
-      <Menu.Item key="edit" onClick={this.showEdit}>
-        Edit
-      </Menu.Item>
-      <Menu.Item key="delete">
-        Delete
-      </Menu.Item>
-      <Menu.Item key="share" onClick={this.showShare}>
-        Share
-      </Menu.Item>
-    </Menu>);
-
-    return (
-      <>
-        <Dropdown overlay={menu} trigger={['click']}>
-          <Button icon={<EllipsisOutlined />} type="link" className="ant-dropdown-link" onClick={e => e.preventDefault()} />
-        </Dropdown>
-        <Modal visible={editVisible} onCancel={this.handleCancel}
-          footer={[
-            <Button key="back" onClick={this.handleCancel}>
-              Cancel
-            </Button>,
-            <Button form="edit-clink" htmlType="submit" key="submit" type="primary" loading={loading} >
-              Submit
-            </Button>,
-          ]}
+  return (
+    <>
+      <Dropdown overlay={menu} trigger={['click']}>
+        <Button icon={<EllipsisOutlined />} type="link" className="ant-dropdown-link" onClick={e => e.preventDefault()} />
+      </Dropdown>
+      <Modal visible={editIsVisible} onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Cancel
+          </Button>,
+          <Button form="edit-clink" htmlType="submit" key="submit" type="primary" loading={isLoading} >
+            Submit
+          </Button>,
+        ]}
+      >
+        <Form {...layout} name="edit-clink" onFinish={onEditFinish} onFinishFailed={onFinishFailed}
+          initialValues={{
+            remember: false,
+          }}
         >
-          <Form {...layout} name="edit-clink" onFinish={this.onEditFinish} onFinishFailed={this.onFinishFailed}
-            initialValues={{
-              remember: false,
-            }}
+          <Form.Item label="Edit Clink Title" name="title"
+            rules={[
+              {
+                required: true,
+                message: 'You haven\'t edited the title',
+              },
+            ]}
           >
-            <Form.Item label="Edit Clink Title" name="title"
-              rules={[
-                {
-                  required: true,
-                  message: 'You haven\'t edited the title',
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-          </Form>
-        </Modal>
-        <Modal visible={shareVisible} onCancel={this.handleCancel}
-          footer={[
-            <Button key="back" onClick={this.handleCancel}>
-              Cancel
-            </Button>,
-            <Button form="share-clink" htmlType="submit" key="submit" type="primary" loading={loading} >
-              Submit
-            </Button>,
-          ]}
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Modal visible={shareIsVisible} onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Cancel
+          </Button>,
+          <Button form="share-clink" htmlType="submit" key="submit" type="primary" loading={isLoading} >
+            Submit
+          </Button>,
+        ]}
+      >
+        <Form {...layout} name="share-clink" onFinish={onShareFinish} onFinishFailed={onFinishFailed}
+          initialValues={{
+            remember: false,
+          }}
         >
-          <Form {...layout} name="share-clink" onFinish={this.onShareFinish} onFinishFailed={this.onFinishFailed}
-            initialValues={{
-              remember: false,
-            }}
+          <Form.Item label="Share write access by username" name="username"
+            rules={[
+              {
+                required: false,
+              },
+            ]}
           >
-            <Form.Item label="Share write access by username" name="username"
-              rules={[
-                {
-                  required: false,
-                },
-              ]}
-            >
-              <Select mode="multiple">
-                <Option value="Placeholder">Placeholder</Option>
-              </Select>
-            </Form.Item>
-            <Form.Item label="Share read access by link" name="link"
-              rules={[
-                {
-                  required: false,
-                },
-              ]}
-            >
-              { this.props.id }
-            </Form.Item>
-          </Form>
-        </Modal>
-      </>
-    );
-  }
+            <Select mode="multiple">
+              <Option value="Placeholder">Placeholder</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Share read access by link" name="link"
+            rules={[
+              {
+                required: false,
+              },
+            ]}
+          >
+            { props.id }
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  );
 }
 
 export default ClinkMenu;
