607fbff351627f29cdb6f42cc75fde412ce4f87b
Claire <claireche@google.com>
2020-07-21 12:48:13 -0400

    display public clinks

--- react-app/src/components/Users/index.js
+++ react-app/src/components/Users/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import '../../index.css';
-import { Layout } from 'antd';
+import { Layout, Collapse } from 'antd';
+import { useSelector, useDispatch } from 'react-redux';
 import Sidebar from '../Common/sidebar';
 import Topbar from '../Common/topbar';
+import ClinkMenu from '../Common/clinkmenu';
 
 const { Content, Footer } = Layout;
+const { Panel } = Collapse;
+
 
 const Users = () => {
+  const clinks = useSelector(state => state.clink.clinks);
+  console.log(clinks)
+
   return (
     <Layout>
       <Sidebar />
@@ -16,6 +23,20 @@ const Users = () => {
         <Content style={{ position: 'relative', margin: '24px 16px 0', overflow: 'initial' }}>
           <div className="site-layout-background" style={{ padding: '24px', textAlign: 'center' }}>
           <h1 className="user-title">Public</h1>
+          {clinks.map(clink => (
+
+            <>
+            {!clink.private &&
+              <>
+                <Collapse accordion className="card" bordered={false}>
+                  <Panel style={{ border: '0px'}} showArrow={false} header={ <div className="card-header">{clink.title}<ClinkMenu /></div>}>
+                  </Panel>
+                </Collapse>
+                <br />
+              </>
+            }
+           </>
+          ))}
           <h1 className="user-title">Private</h1>
           </div>
         </Content>
