ba9c8e3b65466deec88f490e5305ca69b1c4a7c9
Claire <claireche@google.com>
2020-07-21 15:30:04 -0400

    fix private and public clink display

--- flask-server/main.py
+++ flask-server/main.py
@@ -328,13 +328,14 @@ def fetch_clinks():
         all_query.order = ['created']
         all_list = list(all_query.fetch())
         to_return = []    
-
+        print(all_list)
         for clink in all_list:
             for id in clink_ids:
                 if id['clink_id'] == clink.id:
                     response_object = {
                         'title': clink['title'],
-                        'id': clink.id 
+                        'id': clink.id,
+                        'private': clink['private'] 
                     }
                     to_return.append(response_object)
                     break
