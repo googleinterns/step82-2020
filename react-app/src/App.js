40f0d016f7d587039a827c3f46dffb63b66cda94
michaeleggboy <michaeleggboy@google.com>
2020-07-24 16:49:59 -0400

    changed ':clink' to ':clinkId' in App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/get-started/:form?" component={GetStartedPage} />
-          <Route exact path="/dashboard/:clink/:searchParams?" component={Dashboard} />
+          <Route exact path="/dashboard/:clinkId/:searchParams?" component={Dashboard} />
           <Route exact path="/" component={Home} />
           <Route exact path="/users/:userId" component={Users} />
           <Route component={NotFound} />
