b4ffde9bae28fc61c3043714f7fe95217e9f37fd
michaeleggboy <michaeleggboy@google.com>
2020-07-27 12:06:06 -0400

    removed '/' in App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,9 +14,9 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/get-started/:form?" component={GetStartedPage} />
-          <Route exact path="/dashboard/:clinkId/" component={Dashboard} />
+          <Route exact path="/dashboard/:clinkId" component={Dashboard} />
           <Route exact path="/" component={Home} />
-          <Route exact path="/users/:userId/" component={Users} />
+          <Route exact path="/users/:userId" component={Users} />
           <Route component={NotFound} />
         </Switch>
       </div>
