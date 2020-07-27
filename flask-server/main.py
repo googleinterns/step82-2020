ceac3b41303c35be3b1c91e632ded51b966e5bb6
marswang <marswang@google.com>
2020-07-27 12:37:51 -0700

    Added dependencies, modified port and setup deployment.

--- flask-server/main.py
+++ flask-server/main.py
@@ -431,4 +431,8 @@ def fetch_bookmarks(clink_id):
 def catch_all(path):
   return render_template("index.html")
 
-app.run(debug=True)
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
