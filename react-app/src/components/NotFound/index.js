7e3ec46d7d542fc8ce43bd0ddcf9faa545f31297
michaeleggboy <michaeleggboy@google.com>
2020-07-28 07:58:11 -0400

    added actual 404 page

--- react-app/src/components/NotFound/index.js
+++ react-app/src/components/NotFound/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Result, Button } from 'antd';
 
-const NotFound = () => <div>404 - Not Found</div>
+const NotFound = () => {
+
+  return (
+    <Result
+      className="center-load"
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Link to="/dashboard/All">
+          <Button type="primary">Back Home</Button>
+        </Link>
+      }
+    />
+  );
+};
 
 export default NotFound;
