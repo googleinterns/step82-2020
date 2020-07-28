7b1178a94a423e019205ea32ee5f3815d1083270
michaeleggboy <michaeleggboy@google.com>
2020-07-28 15:19:21 -0400

    Revert "added adjustments for development vs. production build"
    This reverts commit 27f5f1e9d93f735c4b2717e2f8a1ab0e4c483d86.

--- flask-server/main.py
+++ flask-server/main.py
@@ -13,14 +13,7 @@ app = Flask(__name__, template_folder='static/react')
 CORS(app)
 bcrypt = Bcrypt()
 
-SECRET_KEY
-
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    SECRET_KEY = os.getenv('SECRET_KEY')
-} else {
-    SECRET_KEY = list(datastore_client.query(kind='SETTINGS').add_filter('name', '=', 'SECRET_KEY').fetch(limit=1))[0]['value']
-}
- 
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # sign-up api
 @app.route('/apis/sign-up', methods=['POST'])
