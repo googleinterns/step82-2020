5e029383d591ce7ca9567aa7f50b51f00c577879
michaeleggboy <michaeleggboy@google.com>
2020-07-28 07:40:47 -0400

    added semicolon

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 import configureStore from './store';
