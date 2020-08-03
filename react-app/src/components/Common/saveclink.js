e2cc06682e52202849660e58ca64d1f74e35a56a
Alisa Lin <36015003+alisa-lin@users.noreply.github.com>
2020-08-03 13:51:26 -0400

    Remove unused imports

--- react-app/src/components/Common/saveclink.js
+++ react-app/src/components/Common/saveclink.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchUsers } from '../../features/users';
 import 'antd/dist/antd.css';
 import '../../index.css';
 import { EllipsisOutlined } from '@ant-design/icons';
