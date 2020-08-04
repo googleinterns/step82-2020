dd822570405c3321f705991c7a49ff5676883ef8
Claire <claireche@google.com>
2020-08-04 11:02:55 -0400

    switch modal form for unsave

--- react-app/src/components/Common/saveclink.js
+++ react-app/src/components/Common/saveclink.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { addReadMap } from '../../features/clink'
+import { addReadMap, unsave } from '../../features/clink'
 import 'antd/dist/antd.css';
 import '../../index.css';
 import { EllipsisOutlined } from '@ant-design/icons';
@@ -16,29 +16,30 @@ const SaveClinkMenu = (props) => {
 
   var menu = (
     <Menu>
-      <Menu.Item key="save" onClick={showSaveConfirm}>
-        Save
+      <Menu.Item key="save" onClick={showConfirm}>
+        {props.display === "unsave" ? 'Unsave' : 'Save'}
       </Menu.Item>
     </Menu>  
   );
+  var titleText = 'Do you want to save this clink?';
+  var contentText = 'You will have read access to this clink.';
 
   if (props.display === "unsave") {
-    menu = (
-      <Menu>
-        <Menu.Item key="edit">
-          Unsave
-        </Menu.Item>
-      </Menu>
-    );
+    titleText = 'Do you want to unsave this clink?';
+    contentText = 'You will not have read access to this clink anymore.';
   }
   
-  function showSaveConfirm() {
+  function showConfirm() {
     confirm({
-      title: 'Do you want to save this clink?',
+      title: titleText,
       icon: <ExclamationCircleOutlined />,
-      content: 'You will have read access to this clink.',
+      content: contentText,
       onOk() {
-        dispatch(addReadMap(props.clink, currentUser));
+        if (props.display === "unsave") {
+          dispatch(unsave(props.clink, currentUser))
+        } else {
+          dispatch(addReadMap(props.clink, currentUser));
+        }
         return new Promise((resolve, reject) => {
           setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
         }).catch(() => console.log('Oops errors!'));
